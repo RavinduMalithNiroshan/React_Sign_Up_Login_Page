9982ec98acd13448877ad8899c47681403dbc38c
RavinduMalithNiroshan <ravindumalith15@gmail.com>
2024-07-11 11:40:14 +0530

    Done
    Complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { LoginSingUp } from './Components/LoginSingUp/LoginSingUp';
+
+function App() {
+  return (
+    <div>
+    <LoginSingUp/>
+    </div>
+  );
+}
+
+export default App;
